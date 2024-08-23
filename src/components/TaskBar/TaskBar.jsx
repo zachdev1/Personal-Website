e5978c69ca58e0a656fc681ec0f7ac8b58904107
zachdev1 <zmsoftdev@gmail.com>
2024-08-23 13:29:09 -0400

    fixed img paths

--- src/components/TaskBar/TaskBar.jsx
+++ src/components/TaskBar/TaskBar.jsx
@@ -5,7 +5,7 @@ const TaskBar = () => {
     return (
         <>  
             <div className='task-bar'>
-                <img src="public/pictures/windows-xp-taskbar.png" alt="" />
+                <img src="pictures/windows-xp-taskbar.png" alt="" />
             </div>
         </>
     );
